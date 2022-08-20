46417aafdeea032dc50b7771fd4219942ced8677
jmrshll <33238796+jmrshll@users.noreply.github.com>
2022-08-20 15:01:29 -0700

    Implemented constructor method for Cell

--- pixelengine/src/main.rs
+++ pixelengine/src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
     println!("Hello, world!");
+
 }
 
 pub struct Cell {
     is_full: bool,
     order: u8,
+}
+
+impl Cell {
+    pub fn new(is_full: bool) -> Self {
+        Self {
+            is_full: is_full,
+            order: 0,
+        }
+    }
 }
 
 pub struct Grid {
