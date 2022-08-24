9f31dcebd3c308fce6e0847ad3afca561201bd33
jmrshll <33238796+jmrshll@users.noreply.github.com>
2022-08-23 20:42:57 -0700

    Implemented the basics of Grid
    Populates a vector with instances of the Cell class (all full rather than randomly empty for now)

--- pixelengine/src/main.rs
+++ pixelengine/src/main.rs
@@ -3,7 +3,6 @@ pub const GRID_HEIGHT: u8 = 24;
 
 fn main() {
     println!("Hello, world!");
-
 }
 
 pub struct Cell {
@@ -15,6 +14,9 @@ impl Cell {
     pub fn new(is_full: bool) -> Self {
         Self {
             is_full: is_full,
+            // order tracks rendering
+            // and cellular automata 
+            // behavioral updating sequence
             order: 0,
         }
     }
@@ -24,4 +26,20 @@ pub struct Grid {
     width: u8,
     height: u8,
     cells: Vec<Cell>,
+}
+
+impl Grid {
+    pub fn new(&mut self, width: u8, height: u8) -> Self {
+        for y in 0..GRID_HEIGHT {
+            for x in 0..GRID_WIDTH {
+                self.cells.push(Cell::new(true));
+            }
+        }
+        
+        Self {
+            width: width,
+            height: height,
+            cells: Vec::<Cell>::new(),
+        }
+    }
 }
